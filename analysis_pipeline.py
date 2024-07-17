0d85ac177f1df3002eb4a3638deafbdbfc88d964
MasoumehGHM <magh1289@colorado.edu>
2024-07-17 17:00:24 +0000

    modifications

--- analysis_pipeline.py
+++ analysis_pipeline.py
@@ -1,7 +1,7 @@
 ########### imports
 import sys
 import numpy as np
-
+import os
 
 ########## functions
 
@@ -25,8 +25,14 @@ def read_data(data_path):
 def mean_std_list(list1):
     return np.mean(list1), np.std(list1)
 
-def largest_three_list(list1, col=1):
-    return sorted(list1, key = lambda x : x[col])[-3:0]
+def top_n_list(list1, n, col=1):
+    return sorted(list1, reverse = True, key = lambda x : x[col])[0:n]
+
+def make_result_file(result, output_path):
+    with open(output_path, 'w') as file:
+        for piece in result:
+            file.write(str(piece) + '\n')
+              
     
 ##########
 # main
@@ -36,16 +42,19 @@ def main():
     if data_path == -1:
         print("No data file is given.")
         return 0 
-    if !exists(data_path):
+    if not os.path.exists(data_path):
         print("The data path does not exits.")
         return 0 
     
     # Get data
     dict = read_data(data_path)
     mu, sigma = mean_std_list(dict['population'])
-    pop_three_largest = largest_three_list(list(zip(dict['county_name'],dict['population'])), col=1)
-    area_three_largest = largest_three_list(list(zip(dict['county_name'],dict['area'])), col=1):
     
+    pop_three_largest = top_n_list(list(zip(dict['county_name'],dict['population'])), 3, col=1)
+    area_three_largest = top_n_list(list(zip(dict['county_name'],dict['area'])), 3, col=1)
+    
+    res = [mu, sigma, pop_three_largest, area_three_largest]
+    make_result_file(res, 'output.txt')
     # Print result
     print(f"population mean:, {mu}!")
     print(f"population stdev:, {sigma}!")
@@ -55,6 +64,10 @@ def main():
     print(area_three_largest)
     
     return 0
+
+###
+if __name__ == "__main__":
+    main()
     
     
         
